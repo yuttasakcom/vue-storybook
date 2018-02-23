6a201de139383d95dea9a95e680e33049161a943
YoProgrammer <yuttasakcom@gmail.com>
2018-02-23 12:09:12 +0700

    create story

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,13 +4,13 @@ import Vue from 'vue'
 // import Vuex from 'vuex' // Vue plugins
 
 // Import your custom components.
-import Mybutton from '../src/components/Button.vue'
+import Button from '../src/components/Button.vue'
 
 // Install Vue plugins.
 // Vue.use(Vuex)
 
 // Register custom components.
-Vue.component('my-button', Mybutton)
+Vue.component('app-button', Button)
 
 function loadStories() {
   // You can require as many stories as you need.
--- src/stories/index.js
+++ src/stories/index.js
@@ -7,7 +7,7 @@ import Button from '../components/Button.vue'
 storiesOf('Button', module)
   .add('with slot', () => ({
     components: { Button },
-    template: "<Button>I'm slot</Button>"
+    template: "<app-button>I'm slot</app-button>" // app-button from .storybook/config.js
   }))
   .add('with event', () => ({
     components: { Button },
